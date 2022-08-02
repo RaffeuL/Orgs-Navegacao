b46c35229a34f4088e887ea7a42bb881c15e2c32
RaffeuL <rafael91.lisboa@hotmail.com>
2022-08-02 15:14:27 -0300

    Estrutura do Produtor

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { StyleSheet, FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
 
-import { Text } from "react-native";
+import Cesta from "./componentes/Cesta";
+
 
 export default function Produtor() {
-    return <Text>Produtor</Text>
+    const route = useRoute();
+    const { nome, imagem, cestas } = route.params;
+
+    return <FlatList 
+        data={cestas}
+        renderItem={({item}) => <Cesta {...item} produtor={{ nome, imagem }}/>}
+        style={estilos.lista}
+        
+    />
 }
+
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+
+        marginTop: -23,
+
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
